4add66e6965785036a442ccbabbece5dfadc26f0
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-05-03 06:51:23 +0200

    track travis

--- Services/PlaceholderProviderService.php
+++ Services/PlaceholderProviderService.php
@@ -4,9 +4,14 @@ class PlaceholderProviderService {
 
     protected $generator;
 
+    public function __construct() {
+
+    }
+
     public function getPlaceholder($inputfile, $outputfile) {
         if (!\file_exists($outputfile) || filemtime($inputfile) < filemtime($outputfile)) {
             $this->generator->generate($inputfile, $outputfile);
         }
+        return $outputfile;
     }
 }
