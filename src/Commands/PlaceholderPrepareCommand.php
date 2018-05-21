7b9d2c4c1c909eded6b88142a45d58bc7f7d3bc3
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-05-21 11:52:51 +0200

    Log the processed images also in dry run of prepare command

--- src/Commands/PlaceholderPrepareCommand.php
+++ src/Commands/PlaceholderPrepareCommand.php
@@ -2,11 +2,11 @@
 
 namespace BernhardWebstudio\PlaceholderBundle\Commands;
 
-use Symfony\Component\Finder\Finder;
+use BernhardWebstudio\PlaceholderBundle\Service\PlaceholderProviderService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use BernhardWebstudio\PlaceholderBundle\Service\PlaceholderProviderService;
+use Symfony\Component\Finder\Finder;
 
 class PlaceholderPrepareCommand extends Command
 {
@@ -24,19 +24,22 @@ class PlaceholderPrepareCommand extends Command
         $this
         // the name of the command (the part after "bin/console")
         ->setName('bewe:placeholder:prepare')
-        ->addOption('dry')
+            ->addOption('dry')
 
         // the short description shown while running "php bin/console list"
-        ->setDescription('Creates placeholders for all the images.')
+            ->setDescription('Creates placeholders for all the images.')
 
         // the full command description shown when running the command with
         // the "--help" option
-        ->setHelp('This command creates the placeholders for all the images in your load_paths');
+            ->setHelp('This command creates the placeholders for all the images in your load_paths');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dry = $input->getOption('dry');
+        if ($dry) {
+            $output->writeln("Dry run. No images will be generated.");
+        }
         $finder = new Finder();
         $finder->name("/\.jpe?g$/")->name('*.png');
         foreach ($this->provider->getLoadPaths() as $path) {
@@ -44,13 +47,13 @@ class PlaceholderPrepareCommand extends Command
         }
 
         $finder->files();
-        if (!$dry) {
-            foreach ($finder as $image) {
+        foreach ($finder as $image) {
+            if (!$dry) {
                 $path = $this->provider->getPlaceholder($image->getRealPath(), PlaceholderProviderService::MODE_PATH);
                 $output->writeln($path . ' created.');
+            } else {
+                $output->writeln($image . ' would have been processed.');
             }
-        } else {
-            $output->writeln("Dry run. No images will be generated.");
         }
 
         $output->writeln("Handled " . count($finder) . ' images.');
