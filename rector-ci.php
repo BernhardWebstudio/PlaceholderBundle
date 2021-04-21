958a741143e8fffc0ff1e5a0902a42fe6a2ca1a4
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-04-21 09:34:16 +0200

    Fix rector sets

--- rector-ci.php
+++ rector-ci.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set('sets', ['dead-code']);
+    $parameters->set('sets', [
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY
+    ]);
 };
