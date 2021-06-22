55124e262b632038cc518d9eda8c21293e6a74db
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-06-22 09:58:02 +0200

    Upgrade Dependencies to latest version

--- rector-ci.php
+++ rector-ci.php
@@ -8,8 +8,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set('sets', [
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY
-    ]);
+    $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
 };
